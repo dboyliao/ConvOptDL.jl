8f20950fb1f2edf9e8c96e163fa3f99f8833aa4c
dboyliao <qmalliao@gmail.com>
2020-07-18 13:58:48 +0800

    WIP

--- src/layers/basic_block.jl
+++ src/layers/basic_block.jl
@@ -3,30 +3,48 @@ using Flux
 export ResNetBasicBlock
 
 struct ResNetBasicBlock
-    init
-    keep_rate::AbstractFloat
-    block_size::Integer
-    stride::Integer
-    pad::Integer
-
+    pre_block::Any
+    downsample_block::Any
+    post_block::Any
+    drop_block::Any
 end
 
-function ResNetBasicBlock(init=Flux.kaiming_normal, keep_rate=0.0, block_size=3)
-    ResNetBasicBlock(init, keep_rate, block_size, 2, 1)
-end
-
-function (block::ResNetBasicBlock)(chs::Pair{Integer, Integer}, downsample=nothing)
+function ResNetBasicBlock(
+    chs::Pair{Integer,Integer};
+    init = Flux.kaiming_normal,
+    drop_prob = 0.0,
+    block_size = 3,
+    stride = 2,
+    pad = 1,
+    downsample = false,
+)
     in_channel, out_channel = chs
-    layers = [
-        Conv((3, 3), in_channel => out_channel, init = block.init, stride = block.stride, pad = block.pad),
+    pre_block = Chain(
+        Conv((3, 3), in_channel => out_channel, init = init, stride = stride, pad = pad),
         BatchNorm(out_channel, (x) -> leakyrelu(x, 0.1)),
-        Conv((3, 3), out_channel => out_channel, init = block.init, stride = block.stride, pad = block.pad),
+        Conv((3, 3), out_channel => out_channel, init = init, stride = stride, pad = pad),
+        BatchNorm(out_channel, (x) -> leakyrelu(x, 0.1)),
+        Conv((3, 3), out_channel => out_channel, init = init, stride = stride, pad = pad),
         BatchNorm(out_channel),
-        Conv((3, 3), out_channel => out_channel, init = block.init, stride = block.stride, pad = block.pad),
-        BatchNorm(out_channel),
-        MaxPool((2, 2)),
-    ]
-    if !isnothing(downsample)
-        
+    )
+    downsample_block = downsample ?
+        Chain(Conv((1, 1), in_channel => out_channel, stride = 1), BatchNorm(out_channel)) :
+        identity
+    post_block = Chain((x)->leakyrelu(x, 0,1), MaxPool((stride, stride), stride=stride))
+    drop_block = DropBlock(block_size, 1 - drop_prob)
+    if drop_prob > 0
+        Flux.trainmode!(drop_block)
+    else
+        Flux.testmode!(drop_block)
     end
+    ResNetBasicBlock(pre_block, downsample_block, post_block, drop_block)
+end
+
+function (block::ResNetBasicBlock)(x)
+    residule = block.downsample_block(x)
+    out = block.pre_block(x)
+    out += residule
+    out = block.post_block(out)
+    out = block.drop_block(out)
+    out
 end
