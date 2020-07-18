c583b063bd5ecc64ad6ec83224768e4aa0836aa0
dboyliao <qmalliao@gmail.com>
2020-07-18 14:36:23 +0800

    WIP

--- src/layers/basic_block.jl
+++ src/layers/basic_block.jl
@@ -21,16 +21,16 @@ function ResNetBasicBlock(
     in_channel, out_channel = chs
     pre_block = Chain(
         Conv((3, 3), in_channel => out_channel, init = init, stride = stride, pad = pad),
-        BatchNorm(out_channel, (x) -> leakyrelu(x, 0.1)),
+        BatchNorm(out_channel, (x) -> leakyrelu(x, Float32(0.1))),
         Conv((3, 3), out_channel => out_channel, init = init, stride = stride, pad = pad),
-        BatchNorm(out_channel, (x) -> leakyrelu(x, 0.1)),
+        BatchNorm(out_channel, (x) -> leakyrelu(x, Float32(0.1))),
         Conv((3, 3), out_channel => out_channel, init = init, stride = stride, pad = pad),
         BatchNorm(out_channel),
     )
     downsample_block = downsample ?
         Chain(Conv((1, 1), in_channel => out_channel, stride = 1), BatchNorm(out_channel)) :
         identity
-    post_block = Chain((x)->leakyrelu(x, 0,1), MaxPool((stride, stride), stride=stride))
+    post_block = Chain((x)->leakyrelu(x, Float32(0.1)), MaxPool((stride, stride), stride=stride))
     drop_block = DropBlock(block_size, 1 - drop_prob)
     if drop_prob > 0
         Flux.trainmode!(drop_block)
@@ -43,7 +43,7 @@ end
 function (block::ResNetBasicBlock)(x)
     residule = block.downsample_block(x)
     out = block.pre_block(x)
-    out += residule
+    out = out .+ residule
     out = block.post_block(out)
     out = block.drop_block(out)
     out
