6ea22356b78da82f2ee50b94be0ea285655d97c7
dboyliao <qmalliao@gmail.com>
2020-07-20 10:22:30 +0800

    WIP

--- src/layers/basic_block.jl
+++ src/layers/basic_block.jl
@@ -14,24 +14,23 @@ function ResNetBasicBlock(
     init = Flux.kaiming_normal,
     drop_prob = 0.0,
     block_size = 3,
-    stride = 2,
-    pad = 1,
+    pool_stride = 2,
     downsample = false,
 )
     in_channel, out_channel = chs
     pre_block = Chain(
-        Conv((3, 3), in_channel => out_channel, init = init, stride = stride, pad = pad),
+        Conv((3, 3), in_channel => out_channel, init = init, stride = 1, pad = 1),
         BatchNorm(out_channel, (x) -> leakyrelu(x, Float32(0.1))),
-        Conv((3, 3), out_channel => out_channel, init = init, stride = stride, pad = pad),
+        Conv((3, 3), out_channel => out_channel, init = init, stride = 1, pad = 1),
         BatchNorm(out_channel, (x) -> leakyrelu(x, Float32(0.1))),
-        Conv((3, 3), out_channel => out_channel, init = init, stride = stride, pad = pad),
+        Conv((3, 3), out_channel => out_channel, init = init, stride = 1, pad = 1),
         BatchNorm(out_channel),
     )
     downsample_block = downsample ?
         Chain(Conv((1, 1), in_channel => out_channel, stride = 1), BatchNorm(out_channel)) :
         identity
     post_block =
-        Chain((x) -> leakyrelu(x, Float32(0.1)), MaxPool((stride, stride), stride = stride))
+        Chain((x) -> leakyrelu(x, Float32(0.1)), MaxPool((pool_stride, pool_stride), stride = pool_stride))
     drop_block = DropBlock(block_size, 1 - drop_prob)
     if drop_prob > 0
         Flux.trainmode!(drop_block)
