dd9b168133fc96249764e527bd548d89ec9fcb45
dboyliao <qmalliao@gmail.com>
2020-07-30 21:16:47 +0800

    update slides

--- presentation/make.jl
+++ presentation/make.jl
@@ -1,10 +1,11 @@
-import Remark
+using Remark, FileWatching
 
-slideshowdir = Remark.slideshow(
-    ".",
-    options = Dict("ratio" => "16:9"),
-    title = "Meta-Learning and Differentiable Convex Optimization with Flux and Zygote",
-)
-
-# # Open presentation in default browser.
-# Remark.open(slideshowdir)
+while true
+    Remark.slideshow(
+        @__DIR__;
+        options = Dict("ratio" => "16:9"),
+        title = "Meta-Learning and Differentiable Convex Optimization with Flux and Zygote",
+    )
+    @info "Rebuilt"
+    FileWatching.watch_folder(joinpath(@__DIR__, "src"))
+end
